45dcdd203c9a510f703585146c25a4e14b7c021c
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-02-03 07:23:31 +0000

    initialisation of git repo

---
+++ starting/coordinates.js
@@ -0,0 +1,27 @@
+const findNewCoordinates = (direction, startingArray, startingPosition) => {
+
+    let finalArray = startingArray;
+    let finalPosition = startingPosition;  
+    
+    if (direction === 'u') {
+        finalArray = finalArray - 1;
+      }
+     else if (direction === 'd'){                  
+        finalArray = finalArray + 1;
+      }
+    else if (direction === 'l') {
+        finalPosition = finalPosition - 1;
+      }
+    else if (direction === 'r'){
+        finalPosition = finalPosition + 1;
+      }
+    
+  
+  return {
+    newArray: finalArray,
+    newPosition: finalPosition  
+  }
+  
+  };
+  
+  module.exports.findNewCoordinates = findNewCoordinates;
